a11b27dc35215e8af6fb53fcd8fc9659ca7c874a
John <John01166@ya.ru>
2022-10-04 15:34:32 +0300

    DZ1

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿/* Console.Write("Введите число");
+int num =  Convert.ToInt32(Console.ReadLine());
+int num1 = - num;
+
+
+while (num1<=num)
+{
+  Console.Write($"{num1}");
+  num1++;
+}*/
+/* int x = 6;
+string  s = "Stroka"; 
+Console.Clear();
+Console.Write("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.Write($"You vveli  {num}");
+Console.WriteLine($" HELLO, WORLD " +  s);
+Console.Write(" C#");
+Console.WriteLine(" C#");
+Console.WriteLine(x);  */
+ Console.Write("Vvedite chislo: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if(num > 5)
+int num2 = num*num;
+
+Console.WriteLine($"Квадрат этого числа = {num2}");
