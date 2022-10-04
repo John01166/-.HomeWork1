453961e7b69d7ab0bd01d0e9d07c3d3c4883f546
John <John01166@ya.ru>
2022-10-04 16:21:48 +0300

    DZ6

--- C#_HW3/Program.cs
+++ C#_HW3/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 6: Напишите программу, которая на вход принимает число
+и выдаёт, является ли число чётным (делится ли оно на два без остатка). */
+
+Console.WriteLine("Введите число, и мы проверим, четное ли оно");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 %2 == 0) 
+    {
+    Console.WriteLine("Число четное");
+    }
+    else
+    {
+    Console.WriteLine("Число нечетное");
+    }
